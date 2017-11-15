91d5e679ef8485197203e0453c688807c867f6f5
Jingsai Liang <jliang@macbook-air-2.devices.wcslc.net>
2017-11-15 16:51:26 -0700

    lab10 change

---
+++ labs/lab10/lab10.py
@@ -0,0 +1,21 @@
+def changemaking(d,n):
+    pass
+        
+def climbStairs(n):
+    pass
+
+def rolldice(N,M):
+    pass
+    
+# Question 1
+d=[1,3,4,5,6]
+n=10
+print changemaking(d,n)
+
+# Question 2
+print climbStairs(30)
+
+# Question 3
+print rolldice(2,7)
+print rolldice(3,9)
+print rolldice(8,24)
